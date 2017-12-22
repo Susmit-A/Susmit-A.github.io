4a42749094cc426377e2b984293c7b478fbb358b
Susmit-A <susmit600@gmail.com>
2017-12-22 12:28:37 +0530

    added python webhook for home assistant

---
+++ home_assistant_fulfilment.py
@@ -0,0 +1,32 @@
+import json
+import os
+import urllib
+from flask import Flask
+from flask import request
+from flask import make_response
+app=Flask(__name__)
+@app.route('/webhook',methods=['POST'])
+def webhook():
+	req = request.get_json(silent=True,force=True)
+	print("Request:")
+	print(json.dumps(req,indent=4))
+	res={
+		"speech": "Complete",
+		"displayText": "Complete",
+		"source": "Myself"
+	}
+	res=json.dumps(res,indent=4)
+	r=make_response(res)
+	r.headers['Content-Type']='application/json'
+	return r
+
+def makeWebHookResult(req):
+	return {
+		"speech": "Complete",
+		"source": "Myself"
+	}
+
+if __name__ == '__main__':
+	port=int(os.getenv('PORT',8080))
+	app.run(port=port,host='localhost',ssl_context='adhoc',debug=False)
+
